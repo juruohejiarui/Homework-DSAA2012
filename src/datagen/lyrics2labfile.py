027d282fe1bac9c48b69afac23e3ccc0c3c14c18
juruohejiarui <1466196957@qq.com>
2025-10-31 18:34:06 +0800

    Add SOFA-Cantonese-Model & and convertion of lyrics

---
+++ src/datagen/lyrics2labfile.py
@@ -0,0 +1,39 @@
+from pycantonese import characters_to_jyutping, characters2jyutping
+import re
+import json
+import argparse
+
+def char_line_to_phonemes(line):
+    # pycantonese returns a space-separated jyutping for each character
+    jy_seq = characters_to_jyutping(line)  # e.g. [('我', 'ngo5'), ('係', 'hai6'), ('香港人', 'hoeng1gong2jan4')]
+
+    # separate each second element of jy_seq by digital (treat digital as the end of each segement) and joins all elements by itertools.chain
+    jy_tokens : list[str] = []
+    for chars, jy in jy_seq :
+        if chars == '，' or chars == '。' or chars == '！' or chars == '？':
+            continue
+        splits = re.split(r'(\d)', jy)
+        for i in range(0, len(splits)-1, 2):
+            jy_tokens.append(splits[i] + splits[i+1])
+    
+    chars = list(line.strip())
+    results = ""
+    for jy in jy_tokens:
+        results += f" {jy[ : -1].strip()}"
+    return results
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", type=str, required=True, help="Input text file with one line of Chinese characters per line.")
+parser.add_argument("--output", type=str, required=True, help="Output lab file.")
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    # 示例
+    line : str = args.input
+    out = char_line_to_phonemes(line)
+    print(json.dumps(out, ensure_ascii=False, indent=2))
+    # 如果你要把每个字的 phoneme 写入 lab 文件，每行一字：
+    with open(args.output, "w", encoding="utf-8") as f:
+       f.write(out.strip() + "\n")
+    print(f"Wrote phonemes to {args.output}")
