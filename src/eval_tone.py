1a1944c69de23e6698f7538d7bd1abc2f14aeb1d
juruohejiarui <1466196957@qq.com>
2025-11-18 23:03:47 +0800

    add test cases

--- src/eval_tone.py
+++ src/eval_tone.py
@@ -25,19 +25,26 @@ if __name__ == "__main__":
 	# curr_notes = ['A', 'B', 'A', 'A', 'A', 'A', 'A', 'G'] # 开不了口 3 # should be 4 3 3 3 3 3 3 3/4
 	# curr_notes = ['D', 'D', 'F', 'G', 'C', 'C', 'C', 'C'] # 开不了口 4 # should be (0 0)/(2 2) 4 3 (4 4 4 4)/(0 0 0 0)
 	# curr_notes = ['E', 'G', 'B', 'A', 'B', 'B', 'B'] # 开不了口 5 # should be 0/2 4 3 3 (3 3)/(4 4)
-	curr_notes = ['A', 'A', 'G', 'G', 'A', 'G'] # should be 3 3 (4 4)/(3 3) 3 4/3 or 2 2 0 0 2 0
+	# curr_notes = ['A', 'A', 'G', 'G', 'A', 'G'] # 开不了口 6 should be 3 3 (4 4)/(3 3) 3 4/3 or 2 2 0 0 2 0
+	# curr_notes = ['D', 'D', 'D', 'D', 'D', 'C'] # 开不了口 6
 	# curr_notes = ['G', 'A', 'c', 'd', 'e', 'e', 'd', 'e'] # 千千阙歌 1 should be 0 2 4 3 3 4 3
 	# curr_notes = ['e', 'd', 'c', 'd', 'c', 'A', 'A'] # 千千阙歌 2 should be 3 3 4 3 3 2 2
 	# curr_notes = ['D', 'D', 'D', 'E', 'F'] # 千千阙歌 3 should be 0 0 0 2 4
 	# curr_notes = ['G', 'A', 'c', 'B', 'B', 'B', 'G', 'E'] # 千千阙歌 4 should be 0 2 3 3 3 3 4 2/0
 	# curr_notes = ['d', 'c', 'd', 'c', 'A', 'c', 'c'] # 千千阙歌 5 should be 3 4 3 4 2 4 4
 	# curr_notes = ['e', 'e', 'd', 'd', 'c', 'd', 'c', 'A'] # 千千阙歌 6 should be 3 3 3 3 4 3 (4 2)/(4 0)/(3 2)
+	curr_notes = ['G', 'A', 'B', 'B', 'A', 'B'] # 千千阙歌 7 should be 4 4 3 3 4 3 / 0 0 2 2 0 2
 	# curr_notes = ['C', 'C', "G", 'G', 'A', 'A', 'G'] # 小星星 1 should be 0 0 4 4 3 3 4
 	# curr_notes = ['F', 'F', "E", 'E', 'D', 'D', 'C'] # 小星星 2 should be 3 3 3 3 3 3 4/3
 	# curr_notes = ['G', 'c', 'B', 'c'] # 必杀技 1 should be 0 4 2 4
 	# curr_notes = ['C', 'D', 'E', 'G', 'G'] # 必杀技 2 should be 0 0 2 (4 4)/(3 3)
+	curr_notes = ['G', 'c', 'c', 'B', 'B', 'A', 'E', 'A'] # 小幸运 1 should be 2/0 3 3 3 3 (3 0 4)/(4 0 4)(3 2 3)
+	curr_notes = ['B', 'e', 'e', 'B', 'B', 'G', 'E', 'G'] # 小幸运 2 should be 2 3 3 3 3 (4 0 4/0)/(3 2 3/4)/(4 2 4)
+	curr_notes = ['e', 'G', 'd', 'e', 'G', 'd', 'e'] # 小幸运 3 should be 3 0 4/3 3 0 4/3 3
+	curr_notes = ['e', 'c', 'c', 'e', 'd', 'c'] # 小幸运 4 should be 3 4 4 3 3 4
 	curr_jyuts = ['1' for _ in range(len(curr_notes))]
 	curr_pitc = data.parse_notelist(curr_notes)
+	print("Input pitches:", curr_pitc[:, 0, 1].tolist())
 	
 	item = data.DataItem(curr_pitc, "x", curr_jyuts)
 	item.setWhole((item.curr_durr, item.curr_pitc, np.zeros((len(item.curr_durr)))))
